c1aaf6dc9666c9c2c2eb004503f3d7b46327e578
Evhenii Mahlena <magellan7777@icloud.com>
2022-06-14 13:43:46 +0300

    Initial Commit

---
+++ YoutubeTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeTask
+//
+//  Created by Evhenii Mahlena on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
